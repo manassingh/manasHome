56417c5974449818c828dad6d12eb1c54074283c
Manas Singh <manas231091iitk@gmail.com>
2016-05-03 20:06:01 +0530

    yo angular started

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name manasHomeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the manasHomeApp
+ */
+angular.module('manasHomeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
