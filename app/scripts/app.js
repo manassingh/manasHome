1107080b5eca6b5cc278a18f77d6fe16f0babbbd
Manas Singh <manas231091iitk@gmail.com>
2016-05-04 02:20:08 +0530

    coming soon page

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name manasHomeApp
+ * @description
+ * # manasHomeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('manasHomeApp', []);
---
+++ app/scripts/controllers/comingsoon.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name manasHomeApp.controller:ComingsoonCtrl
+ * @description
+ * # ComingsoonCtrl
+ * Controller of the manasHomeApp
+ */
+angular.module('manasHomeApp')
+  .controller('ComingsoonCtrl', function () {
+    
+  });
