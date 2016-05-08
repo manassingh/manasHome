e805042bcac6aa20a1528ec86ebff72daa671829
Manas Singh <manas231091iitk@gmail.com>
2016-05-08 21:17:01 +0530

    home page complete only liks dalne hein

---
+++ app/scripts/controllers/footer.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name manasHomeApp.controller:FooterCtrl
+ * @description
+ * # FooterCtrl
+ * Controller of the manasHomeApp
+ */
+angular.module('manasHomeApp')
+  .controller('FooterCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
